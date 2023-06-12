6ce1053c36eab700d304f7cfebaec9ed6776eaa5
Dixita-Tntra <dixita.kataria@tntra.io>
2023-06-12 14:37:40 +0530

    forgot & reset password

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
-    invitations: 'users/invitations'
+    invitations: 'users/invitations',
+    passwords: 'users/passwords'
   }
   devise_scope :user do
     post 'users/invitation/accept_invite' => 'users/invitations#accept_invite'
