ae1d1a5125ec3d0240edf30fc55ab0e05d8b407a
Dixita-Tntra <dixita.kataria@tntra.io>
2023-06-13 14:52:10 +0530

    fixed enum value

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,4 @@
 class Order < ApplicationRecord
   has_many :histories
-  # enum order_type: { 'new': 0, 'repeat': 1, 'design_change': 2 }
+  enum order_type: { 'new_order': 0, 'repeat': 1, 'design_change': 2 }
 end
